1dfdbe6ac9482baad5a30daac5e77db4cd4c39c6
kimcipher <kimutaij192@gmail.com>
2023-03-13 09:57:44 +0300

    error handling

--- bill.py
+++ bill.py
@@ -1,31 +1,58 @@
+# Define constants
+RATE_A = 0.50
+RATE_B = 1.00
+RATE_C = 1.20
+RATE_D = 1.50
+
+ERROR_MESSAGE = "Please enter a valid input."
+USAGE_ERROR_MESSAGE = "Usage must be a positive number."
+ID_ERROR_MESSAGE = "Huduma ID number must be a valid number."
+
 # Get user input
 print("ELECTRICITY BILL BASED ON UNITS CONSUMED")
-house = str(input("Which house do you live in?"))
-renter = str(input("What's your last name as appeared in ID?"))
-socialId = int(input("Enter your social ID number:"))
-usage = float(input("Enter the electricity units:"))
+house = input("Which house do you live in? ")
+renter = input("What's your last name as appeared in ID? ")
 
-# Rates defining
-rateA = 0.50
-rateB = 1.00
-rateC = 1.20
-rateD = 1.50
+# Validate social ID number
+while True:
+    hudumaId_str = input("Enter the Huduma ID number: ")
+    try:
+        hudumaId = int(hudumaId_str)
+        break
+    except ValueError:
+        print(ID_ERROR_MESSAGE)
 
-# Calculating cost based on usage
+# Validate usage
+while True:
+    usage_str = input("Enter the electricity units: ")
+    try:
+        usage = float(usage_str)
+        if usage < 0:
+            print(USAGE_ERROR_MESSAGE)
+        else:
+            break
+    except ValueError:
+        print(USAGE_ERROR_MESSAGE)
 
-if usage > 0 and usage <= 50:
-    total = usage * rateA
+# Calculate cost based on usage
+if usage <= 50:
+    total = usage * RATE_A
     print("{}'s cost is {:.2f}".format(renter, total))
-elif usage > 50 and usage <= 100:
-    total = usage * rateB
+elif usage <= 100:
+    total = usage * RATE_B
     print("{}'s cost is {:.2f}".format(renter, total))
-elif usage > 100 and usage <= 200:
-    total = usage * rateC
+elif usage <= 200:
+    total = usage * RATE_C
     print("{}'s cost is {:.2f}".format(renter, total))
-elif usage > 200 and usage <= 250:
-    total = usage * rateD
+elif usage <= 250:
+    total = usage * RATE_D
     print("{}'s cost is {:.2f}".format(renter, total))
 else:
-    print("{}'s usage of {} is not valid for house {}. Please enter a valid usage.".format(renter, usage, house))e))
+    print("{}'s usage of {} is not valid for house {}. Please enter a valid usage.".format(renter, usage, house))
 
-
+# Print summary of input and output
+print("House:", house)
+print("Renter:", renter)
+print("Huduma ID number:", hudumaId)
+print("Usage:", usage)
+print("Total cost:", total)
