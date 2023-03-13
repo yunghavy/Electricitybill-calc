55fa94a1be50c96ea071e16b6f4c5fe1a28410b1
kimcipher <kimutaij192@gmail.com>
2023-03-13 10:08:44 +0300

    creating functions

--- bill.py
+++ bill.py
@@ -1,58 +1,62 @@
-# Define constants
-RATE_A = 0.50
-RATE_B = 1.00
-RATE_C = 1.20
-RATE_D = 1.50
-
+# Defining constants
 ERROR_MESSAGE = "Please enter a valid input."
 USAGE_ERROR_MESSAGE = "Usage must be a positive number."
 ID_ERROR_MESSAGE = "Huduma ID number must be a valid number."
 
-# Get user input
+RATE_TABLE = {
+    (0, 50): 0.50,
+    (51, 100): 1.00,
+    (101, 200): 1.20,
+    (201, 250): 1.50,
+}
+
+# Defining functions
+def calculate_total_cost(usage):
+    for range_, rate in RATE_TABLE.items():
+        if usage >= range_[0] and usage <= range_[1]:
+            return usage * rate
+    return None
+
+# Getting user input
 print("ELECTRICITY BILL BASED ON UNITS CONSUMED")
-house = input("Which house do you live in? ")
-renter = input("What's your last name as appeared in ID? ")
+num_houses = int(input("How many houses do you want to calculate? "))
 
-# Validate social ID number
-while True:
-    hudumaId_str = input("Enter the Huduma ID number: ")
-    try:
-        hudumaId = int(hudumaId_str)
-        break
-    except ValueError:
-        print(ID_ERROR_MESSAGE)
+for i in range(num_houses):
+    print(f"\nHOUSE {i+1}")
+    house = input("Which house do you live in? ")
+    renter = input("What's your last name as appeared in ID? ")
 
-# Validate usage
-while True:
-    usage_str = input("Enter the electricity units: ")
-    try:
-        usage = float(usage_str)
-        if usage < 0:
-            print(USAGE_ERROR_MESSAGE)
-        else:
+    # Validating huduma ID number
+    while True:
+        hudumaId_str = input("Enter the huduma ID number: ")
+        try:
+            hudumaId = int(hudumaId_str)
             break
-    except ValueError:
-        print(USAGE_ERROR_MESSAGE)
+        except ValueError:
+            print(ID_ERROR_MESSAGE)
 
-# Calculate cost based on usage
-if usage <= 50:
-    total = usage * RATE_A
-    print("{}'s cost is {:.2f}".format(renter, total))
-elif usage <= 100:
-    total = usage * RATE_B
-    print("{}'s cost is {:.2f}".format(renter, total))
-elif usage <= 200:
-    total = usage * RATE_C
-    print("{}'s cost is {:.2f}".format(renter, total))
-elif usage <= 250:
-    total = usage * RATE_D
-    print("{}'s cost is {:.2f}".format(renter, total))
-else:
-    print("{}'s usage of {} is not valid for house {}. Please enter a valid usage.".format(renter, usage, house))
+    # Validating usage
+    while True:
+        usage_str = input("Enter the electricity units: ")
+        try:
+            usage = float(usage_str)
+            if usage < 0:
+                print(USAGE_ERROR_MESSAGE)
+            else:
+                break
+        except ValueError:
+            print(USAGE_ERROR_MESSAGE)
 
-# Print summary of input and output
-print("House:", house)
-print("Renter:", renter)
-print("Huduma ID number:", hudumaId)
-print("Usage:", usage)
-print("Total cost:", total)
+    # Calculate total cost based on usage
+    total = calculate_total_cost(usage)
+    if total is not None:
+        print(f"{renter}'s cost is {total:.2f}")
+    else:
+        print(f"{renter}'s usage of {usage} is not valid for house {house}. Please enter a valid usage.")
+
+    # Print summary of input and output
+    print(f"House: {house}")
+    print(f"Renter: {renter}")
+    print(f"Social ID number: {hudumaId}")
+    print(f"Usage: {usage}")
+    print(f"Total cost: {total:.2f}" if total is not None else "N/A")
